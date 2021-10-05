b7d1d17920ec2dd65acb155c15bb8017786b31ef
kyle-shk <guqtls85@naver.com>
2021-10-05 15:52:03 +0900

    모듈전

--- 입출력/다양한출력포맷.py
+++ 입출력/다양한출력포맷.py
@@ -1,3 +1,4 @@
+import random
 # 빈자리는 빈공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간을 확보
 
 # print('{0: >10}'.format(500))
@@ -7,3 +8,5 @@
 # print('{0: >+10}'.format(-500))
 arr = [1,2,3,4,5,6]
 print(arr[-4])
+
+print(random.randint(1,20))
